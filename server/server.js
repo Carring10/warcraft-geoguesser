1d82eeed244e571a47baf4dc67ad7bca4f98709a
Carrington <hartis.c99@gmail.com>
2023-02-02 13:01:03 -0500

    updated environment variables, connection to the database

--- server/server.js
+++ server/server.js
@@ -3,6 +3,8 @@ const express = require("express");
 const PORT = process.env.PORT || 3001;
 const app = express();
 
+const connection = require('./config/connection');
+
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
 
