7ed1de89cba0411b1787bb8582ed821fe9d3b731
Carrington <hartis.c99@gmail.com>
2023-02-02 15:27:53 -0500

    user model method to create a new user

--- server/controllers/userController.js
+++ server/controllers/userController.js
@@ -1,7 +1,16 @@
+const User = require('../models/User');
+
 exports.getAllUsers = async (req, res, next) => {
   res.send("Get all users route");
 }
 
 exports.createNewUser = async (req, res, next) => {
+  let { username, score } = req.body;
+  let user = new User(username, score);
+
+  user = await user.save();
+
+  console.log(user);
+
   res.send("Create user route");
 }
---
+++ server/models/User.js
@@ -0,0 +1,33 @@
+const db = require('../config/connection');
+
+class User {
+  constructor(username, score) {
+    this.username = username;
+    this.score = score;
+  }
+  // Save new user to the database
+  async save() {
+    let sql = `
+    INSERT INTO users(
+      username,
+      score
+    )
+    VALUES(
+      '${this.username}',
+      '${this.score}'
+    )
+    `;
+
+    const newUser = await db.execute(sql);
+
+    return newUser;
+  }
+
+  static findAll() {
+    let sql = "SELECT * FROM users;";
+
+    return db.execute(sql);
+  }
+}
+
+module.exports = User;
