c0e02b17c7d8f345462bfbbc7e93dea1438badf4
Carrington <hartis.c99@gmail.com>
2023-02-07 13:31:50 -0500

    started on the array of zones

---
+++ client/script.js
@@ -0,0 +1,43 @@
+const zones = [
+  // Kalimdor
+  {
+    name: 'Teldrassil',
+    img: '',
+    hint: "Home to the Night Elves."
+  },
+  {
+    name: 'Darkshore',
+    img: '',
+    hint: "It's long strip of coast on the northwestern part of Kalimdor."
+  },
+  {
+    name: 'Moonglade',
+    img: '',
+    hint: "Serves as a haven for druids, and is also the home of the Cenarion Circle."
+  },
+  {
+    name: 'Winterspring',
+    img: '',
+    hint: "The only settlement here is Everlook."
+  },
+  {
+    name: 'Felwood',
+    img: '',
+    hint: "It is a corrupted forest filled with tainted creatures of the woods, demonic satyr and infernals, and rotting treants and ancients."
+  },
+  {
+    name: 'Felwood',
+    img: '',
+    hint: "It is a corrupted forest filled with tainted creatures of the woods, demonic satyr and infernals, and rotting treants and ancients."
+  },
+  {
+    name: 'Ashenvale',
+    img: '',
+    hint: "This is one of the first contested zones new players on PvP servers will come across on kalimdor."
+  },
+  {
+    name: 'Azshara',
+    img: '',
+    hint: "This zone shares the name with the queen of the Naga."
+  },
+]
