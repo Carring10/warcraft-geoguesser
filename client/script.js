d8d2faab48c82ca19eaeadbf2113183d43be2202
Carrington <hartis.c99@gmail.com>
2023-02-08 18:42:15 -0500

    input field

--- client/script.js
+++ client/script.js
@@ -2,69 +2,78 @@ const zones = [
   // Kalimdor
   {
     name: 'Teldrassil',
-    img: '',
+    img: 'placeholder',
     hint: "Home to the Night Elves."
   },
   {
     name: 'Darkshore',
-    img: '',
+    img: 'placeholder',
     hint: "It's long strip of coast on the northwestern part of Kalimdor."
   },
   {
     name: 'Moonglade',
-    img: '',
+    img: 'placeholder',
     hint: "Serves as a haven for druids, and is also the home of the Cenarion Circle."
   },
   {
     name: 'Winterspring',
-    img: '',
+    img: 'placeholder',
     hint: "The only settlement here is Everlook."
   },
   {
     name: 'Felwood',
-    img: '',
+    img: 'placeholder',
     hint: "It is a corrupted forest filled with tainted creatures of the woods, demonic satyr and infernals, and rotting treants and ancients."
   },
   {
     name: 'Felwood',
-    img: '',
+    img: 'placeholder',
     hint: "It is a corrupted forest filled with tainted creatures of the woods, demonic satyr and infernals, and rotting treants and ancients."
   },
   {
     name: 'Ashenvale',
-    img: '',
+    img: 'placeholder',
     hint: "This is one of the first contested zones new players on PvP servers will come across on kalimdor."
   },
   {
     name: 'Azshara',
-    img: '',
+    img: 'placeholder',
     hint: "This zone shares the name with the queen of the Naga."
   },
 ];
 
 const startScreen = document.querySelector('.start-screen');
 const beginQuiz = document.getElementById('start-button');
-const quizContainer = document.querySelector('.quiz-container');
+const gameContainer = document.querySelector('.game-container');
 
-function hideQuiz() {
-  quizContainer.style.display = 'none';
+function hideGame() {
+  gameContainer.style.display = 'none';
 }
 
-hideQuiz();
+hideGame();
 
 beginQuiz.addEventListener('click', function (event) {
   event.preventDefault();
   startScreen.style.display = 'none';
-  quizContainer.style.display = 'block';
+  gameContainer.style.display = 'block';
 });
 
 function displayZone() {
   const randomizeZones = [...zones].sort(() => 0.5 - Math.random());
   randomizeZones.shift();
-  console.log(randomizeZones);
   const chosenZone = randomizeZones[0];
-  console.log(chosenZone);
   const zoneName = chosenZone.name;
+
+  const img = document.createElement('p');
+  img.innerHTML = zoneName;
+  gameContainer.appendChild(img);
+
+  for (let i = 0; i < zoneName.length; i++) {
+    const input = document.createElement('input');
+    gameContainer.appendChild(input);
+    input.setAttribute('type', 'text');
+    input.setAttribute('maxlength', '1');
+  }
 }
 
 displayZone();
