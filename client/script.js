33ff64d0f37a61d029a0a4b65e5454ced87dbf3c
Carrington <hartis.c99@gmail.com>
2023-02-14 16:24:33 -0500

    refactored functions, moved dynamically created elements into html

--- client/script.js
+++ client/script.js
@@ -1,15 +1,14 @@
 import { zones } from './zones.js';
 
-// fetch('http://localhost:3001/users')
-//   .then(res => res.json())
-//   .then(data => console.log(data))
-
 const startScreen = document.querySelector('.start-screen');
-const beginQuiz = document.getElementById('start-button');
+const start = document.getElementById('start-button');
 const gameContainer = document.querySelector('.game-container');
-const zoneContainer = document.createElement('div');
-const randomizeZones = [...zones].sort(() => 0.5 - Math.random());
-let currentZone = randomizeZones[0];
+const zoneName = document.getElementById('zone-name');
+const submit = document.getElementById('submit');
+const next = document.getElementById('next');
+
+const shuffleZones = [...zones].sort(() => 0.5 - Math.random());
+let currentZone = shuffleZones[0];
 
 function hideGame() {
   gameContainer.style.display = 'none';
@@ -17,36 +16,24 @@ function hideGame() {
 
 hideGame();
 
-beginQuiz.addEventListener('click', function (event) {
-  event.preventDefault();
+start.addEventListener('click', startGame);
 
+function startGame() {
   startScreen.style.display = 'none';
   gameContainer.style.display = 'block';
-});
 
-function displayZone(zone) {
-  randomizeZones.shift();
-  const img = document.createElement('p');
-
-  img.innerHTML = zone.name;
-  gameContainer.appendChild(zoneContainer);
-  zoneContainer.appendChild(img);
+  getNextZone()
 }
 
-displayZone(currentZone);
+function getNextZone() {
+  currentZone = shuffleZones[0];
+  showZone(currentZone);
+}
 
-function handleInput(zone) {
-  const inputFields = document.createElement('div');
-  const submit = document.createElement('button');
-  let userInput = [];
+function showZone(zone) {
+  zoneName.innerHTML = zone.name;
 
-  zoneContainer.appendChild(inputFields);
-  zoneContainer.appendChild(submit);
-
-  inputFields.setAttribute('id', 'input-fields');
-  submit.setAttribute('id', 'submit-button');
-
-  submit.textContent = 'Submit';
+  const inputFields = document.getElementById('input-fields');
 
   for (let i = 0; i < zone.name.length; i++) {
     const input = document.createElement('input');
@@ -71,46 +58,26 @@ function handleInput(zone) {
       }
     }
   }
-  // Submit letters from input
-  submit.addEventListener('click', function () {
-    const inputs = document.querySelectorAll('#input');
-    userInput = [];
+  submit.addEventListener('click', handleInput);
+}
 
-    inputs.forEach((input) => {
-      userInput.push(input.value);
-    });
-    // Check user's answer
-    const inputStr = userInput.toString().replaceAll(',', '').toLowerCase();
-    const answerKey = zone.name.toLowerCase();
+function handleInput() {
+  const inputs = document.querySelectorAll('#input');
+  let userInput = [];
 
-    if (inputStr === answerKey) {
-      console.log('correct');
-      createNextBtn();
-    } else if (inputStr !== '') {
-      console.log('incorrect')
-      createNextBtn();
-    }
-    console.log(userInput)
-    // Next zone
-    function createNextBtn() {
-      const nextBtn = document.createElement('button');
-      zoneContainer.appendChild(nextBtn);
-      nextBtn.textContent = 'Next';
-
-      nextBtn.addEventListener('click', function () {
-        if (inputStr !== '') {
-          getNextZone();
-        }
-      });
-    }
+  inputs.forEach((input) => {
+    userInput.push(input.value);
   });
+  // Check user's answer
+  const inputStr = userInput.toString().replaceAll(',', '').toLowerCase();
+  const answerKey = currentZone.name.toLowerCase();
+
+  if (inputStr === answerKey) {
+    console.log('correct');
+  } else if (inputStr !== '') {
+    console.log('incorrect')
+  }
+  console.log(userInput);
+
+  next.style.display = 'display';
 }
-
-function getNextZone() {
-  currentZone = randomizeZones[0];
-
-  displayZone(currentZone);
-  handleInput(currentZone);
-}
-
-handleInput(currentZone);
