30c0129a5c2b8aced3c3bb866432507a3953d933
Carrington <hartis.c99@gmail.com>
2023-04-05 16:00:00 -0400

    updated some zone images

--- client/zones.js
+++ client/zones.js
@@ -57,7 +57,7 @@ export const zones = [
   },
   {
     name: 'Mulgore',
-    img: 'images/zones/mulgore.jpg',
+    img: 'images/zones/mulgore.webp',
     hint: "This zone is home to the instance Maraudon."
   },
   {
@@ -91,114 +91,114 @@ export const zones = [
     hint: "In this zone, players can buy Noggenfogger Elixir, an unpredictable transformation elixir, from Marin Noggenfogger."
   },
   // Eastern Kingdoms
-  {
-    name: 'Tirisfal Glades',
-    img: 'images/zones/darkshore.jpg',
-    hint: "The capital city of the Forsaken, Undercity, is located here."
-  },
-  {
-    name: 'Western Plaguelands',
-    img: 'images/zones/western-plaguelands.jpg',
-    hint: "This zone is home to the Scholomance instance."
-  },
-  {
-    name: 'Eastern Plaguelands',
-    img: 'images/zones/eastern-plaguelands.jpg',
-    hint: "The Argent Dawn maintains a small camp in Light Hope's Chapel, the only fully friendly settlement in the area."
-  },
-  {
-    name: 'The Hinterlands',
-    img: 'images/zones/the-hinterlands.jpg',
-    hint: "This is a grassy mountainous zone in northern Eastern Kingdoms."
-  },
-  {
-    name: 'Alterac Mountains',
-    img: 'images/zones/alterac-mountains.jpg',
-    hint: "Residing here is Dalaran, a mage city controlled by the Kirin Tor."
-  },
-  {
-    name: 'Silverpine Forest',
-    img: 'images/zones/silverpine-forest.jpg',
-    hint: "Home to the dungeon, Shadowfang Keep."
-  },
-  {
-    name: 'Hillsbrad Foothills',
-    img: 'images/zones/hillsbrad-foothills.jpg',
-    hint: "This zone, covered in grassy hills and plenty of wildlife, is one of the only locations of Lordaeron that was left untouched by the Scourge."
-  },
-  {
-    name: 'Arathi Highlands',
-    img: 'images/zones/arathi-highlands.jpg',
-    hint: "Stromgarde, the former capital of the kingdom of Arathor, has fallen in disrepair."
-  },
-  {
-    name: 'Wetlands',
-    img: 'images/zones/wetlands.jpg',
-    hint: "This zone is covered in small lakes, rivers and ponds, and houses creatures usually found on a swamp such as crocolisks, murlocs, and bog lords."
-  },
-  {
-    name: 'Loch Modan',
-    img: 'images/zones/loch-modan.jpg',
-    hint: "This zone is mainly known for its namesake lake and the Stonewrought Dam, a massive dam with dwarven faces etched onto its walls."
-  },
-  {
-    name: 'Dun Morogh',
-    img: 'images/zones/dun-morogh.jpg',
-    hint: "It is the starting zone for Dwarves and Gnomes, and contains both the starting area, Coldridge Valley, and the capitals for both races: Ironforge."
-  },
-  {
-    name: 'Searing Gorge',
-    img: 'images/zones/searing-gorge.jpg',
-    hint: "This zone is mainly dominated by Dark Iron Dwarves, who control part of Blackrock Mountain."
-  },
-  {
-    name: 'Badlands',
-    img: 'images/zones/badlands.jpg',
-    hint: "This is a harsh zone with dry earth, mountain ranges and many canyons, it is also home to the Uldaman dungeon."
-  },
-  {
-    name: 'Burning Steppes',
-    img: 'images/zones/burning-steppes.jpg',
-    hint: "This is a rugged region full of crags and cliffs, and as harsh as the zone is, it serves as one of the only roads that connect Stormwind with the dwarven kingdom of Khaz Modan."
-  },
-  {
-    name: 'Redridge Mountains',
-    img: 'images/zones/redridge-mountains.jpg',
-    hint: "This zone is an idyllic region of rivers and and risen elevations in the southern part of the Eastern Kingdoms."
-  },
-  {
-    name: 'Elwynn Forest',
-    img: 'images/zones/elwynn-forest.jpg',
-    hint: "The fearsome Hogger, king of the Gnolls, lives here. Hogger is much stronger than a mob of similar level, and there are many stories of players being slaughtered by him."
-  },
-  {
-    name: 'Swamp of Sorrows',
-    img: 'images/zones/swamp-of-sorrows.jpg',
-    hint: "This zone is the location of The Temple of Atal'Hakkar and is intended for level 36-43 players."
-  },
-  {
-    name: 'Blasted Lands',
-    img: 'images/zones/blasted-lands.jpg',
-    hint: "It is home to the original Dark Portal, used by Medivh to unleash the Orcs into Azeroth for the first time."
-  },
-  {
-    name: 'Deadwind Pass',
-    img: 'images/zones/deadwind-pass.jpg',
-    hint: "This is a haunted forest and canyon on the edges of the Kingdom of Stormwind in the southern Eastern Kingdoms."
-  },
-  {
-    name: 'Duskwood',
-    img: 'images/zones/duskwood.jpg',
-    hint: "Here, many high level elites roam the area - The abomination Stitches is known to roam the way to the zone's main hub, Darkshire, and Mor'Ladim haunts the Raven Hill Cemetery."
-  },
-  {
-    name: 'Westfall',
-    img: 'images/zones/westfall.jpg',
-    hint: "This zone is the home of The Deadmines."
-  },
-  {
-    name: 'Stranglethorn Vale',
-    img: 'images/zones/stranglethorn-vale.jpg',
-    hint: "This zone is home to many events: Every 3 hours, an all-out PvP event is hosted in the Gurubashi Arena, and every Sunday, there is a fishing contest in Booty Bay!"
-  }
+  // {
+  //   name: 'Tirisfal Glades',
+  //   img: 'images/zones/darkshore.jpg',
+  //   hint: "The capital city of the Forsaken, Undercity, is located here."
+  // },
+  // {
+  //   name: 'Western Plaguelands',
+  //   img: 'images/zones/western-plaguelands.jpg',
+  //   hint: "This zone is home to the Scholomance instance."
+  // },
+  // {
+  //   name: 'Eastern Plaguelands',
+  //   img: 'images/zones/eastern-plaguelands.jpg',
+  //   hint: "The Argent Dawn maintains a small camp in Light Hope's Chapel, the only fully friendly settlement in the area."
+  // },
+  // {
+  //   name: 'The Hinterlands',
+  //   img: 'images/zones/the-hinterlands.jpg',
+  //   hint: "This is a grassy mountainous zone in northern Eastern Kingdoms."
+  // },
+  // {
+  //   name: 'Alterac Mountains',
+  //   img: 'images/zones/alterac-mountains.jpg',
+  //   hint: "Residing here is Dalaran, a mage city controlled by the Kirin Tor."
+  // },
+  // {
+  //   name: 'Silverpine Forest',
+  //   img: 'images/zones/silverpine-forest.jpg',
+  //   hint: "Home to the dungeon, Shadowfang Keep."
+  // },
+  // {
+  //   name: 'Hillsbrad Foothills',
+  //   img: 'images/zones/hillsbrad-foothills.jpg',
+  //   hint: "This zone, covered in grassy hills and plenty of wildlife, is one of the only locations of Lordaeron that was left untouched by the Scourge."
+  // },
+  // {
+  //   name: 'Arathi Highlands',
+  //   img: 'images/zones/arathi-highlands.jpg',
+  //   hint: "Stromgarde, the former capital of the kingdom of Arathor, has fallen in disrepair."
+  // },
+  // {
+  //   name: 'Wetlands',
+  //   img: 'images/zones/wetlands.jpg',
+  //   hint: "This zone is covered in small lakes, rivers and ponds, and houses creatures usually found on a swamp such as crocolisks, murlocs, and bog lords."
+  // },
+  // {
+  //   name: 'Loch Modan',
+  //   img: 'images/zones/loch-modan.jpg',
+  //   hint: "This zone is mainly known for its namesake lake and the Stonewrought Dam, a massive dam with dwarven faces etched onto its walls."
+  // },
+  // {
+  //   name: 'Dun Morogh',
+  //   img: 'images/zones/dun-morogh.jpg',
+  //   hint: "It is the starting zone for Dwarves and Gnomes, and contains both the starting area, Coldridge Valley, and the capitals for both races: Ironforge."
+  // },
+  // {
+  //   name: 'Searing Gorge',
+  //   img: 'images/zones/searing-gorge.jpg',
+  //   hint: "This zone is mainly dominated by Dark Iron Dwarves, who control part of Blackrock Mountain."
+  // },
+  // {
+  //   name: 'Badlands',
+  //   img: 'images/zones/badlands.jpg',
+  //   hint: "This is a harsh zone with dry earth, mountain ranges and many canyons, it is also home to the Uldaman dungeon."
+  // },
+  // {
+  //   name: 'Burning Steppes',
+  //   img: 'images/zones/burning-steppes.jpg',
+  //   hint: "This is a rugged region full of crags and cliffs, and as harsh as the zone is, it serves as one of the only roads that connect Stormwind with the dwarven kingdom of Khaz Modan."
+  // },
+  // {
+  //   name: 'Redridge Mountains',
+  //   img: 'images/zones/redridge-mountains.jpg',
+  //   hint: "This zone is an idyllic region of rivers and and risen elevations in the southern part of the Eastern Kingdoms."
+  // },
+  // {
+  //   name: 'Elwynn Forest',
+  //   img: 'images/zones/elwynn-forest.jpg',
+  //   hint: "The fearsome Hogger, king of the Gnolls, lives here. Hogger is much stronger than a mob of similar level, and there are many stories of players being slaughtered by him."
+  // },
+  // {
+  //   name: 'Swamp of Sorrows',
+  //   img: 'images/zones/swamp-of-sorrows.jpg',
+  //   hint: "This zone is the location of The Temple of Atal'Hakkar and is intended for level 36-43 players."
+  // },
+  // {
+  //   name: 'Blasted Lands',
+  //   img: 'images/zones/blasted-lands.jpg',
+  //   hint: "It is home to the original Dark Portal, used by Medivh to unleash the Orcs into Azeroth for the first time."
+  // },
+  // {
+  //   name: 'Deadwind Pass',
+  //   img: 'images/zones/deadwind-pass.jpg',
+  //   hint: "This is a haunted forest and canyon on the edges of the Kingdom of Stormwind in the southern Eastern Kingdoms."
+  // },
+  // {
+  //   name: 'Duskwood',
+  //   img: 'images/zones/duskwood.jpg',
+  //   hint: "Here, many high level elites roam the area - The abomination Stitches is known to roam the way to the zone's main hub, Darkshire, and Mor'Ladim haunts the Raven Hill Cemetery."
+  // },
+  // {
+  //   name: 'Westfall',
+  //   img: 'images/zones/westfall.jpg',
+  //   hint: "This zone is the home of The Deadmines."
+  // },
+  // {
+  //   name: 'Stranglethorn Vale',
+  //   img: 'images/zones/stranglethorn-vale.jpg',
+  //   hint: "This zone is home to many events: Every 3 hours, an all-out PvP event is hosted in the Gurubashi Arena, and every Sunday, there is a fishing contest in Booty Bay!"
+  // }
 ];
