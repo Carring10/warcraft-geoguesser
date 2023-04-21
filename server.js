7d00ce9c26bdf5b1b023d352bf4af69b2dd7d73c
Carrington <hartis.c99@gmail.com>
2023-04-21 19:25:45 -0400

    moved package.json and server.js into root

--- server.js
+++ server.js
@@ -5,7 +5,7 @@ const cors = require('cors');
 const PORT = process.env.PORT || 3001;
 const app = express();
 
-const connection = require('./config/connection');
+const connection = require('./server/config/connection');
 
 // Middleware
 app.use(express.urlencoded({ extended: false }));
@@ -13,7 +13,7 @@ app.use(express.json());
 app.use(cors());
 
 // Requests
-app.use("/users", require("./routes/userRoutes"));
+app.use("/users", require("./server/routes/userRoutes"));
 
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
