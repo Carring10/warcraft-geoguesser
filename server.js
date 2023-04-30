e46cae1c8d0f0b0f7d3054fc4b4a721a608786e6
Carrington <hartis.c99@gmail.com>
2023-04-30 19:50:10 -0400

    node error

--- server.js
+++ server.js
@@ -20,8 +20,7 @@ app.listen(PORT, () => {
 });
 
 // Condition for production 
-
-  app.use(express.static("build"));
-  app.get("*", (req, res) => {
-    res.sendFile(path.resolve(__dirname,  "build", "index.html"));
-  });
+app.use(express.static("build"));
+app.get("*", (req, res) => {
+  res.sendFile(path.resolve(__dirname, "index.html"));
+});
