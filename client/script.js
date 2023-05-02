36be8634693301a4e45651a421eab7e3f125d629
Carrington <hartis.c99@gmail.com>
2023-05-02 11:43:41 -0400

    error

--- client/script.js
+++ client/script.js
@@ -481,7 +481,7 @@ async function updateScore() {
 async function getAllPlayerData() {
   try {
     // Query database.
-    const response = await fetch('/users');
+    const response = await fetch('/');
     console.log(response)
     const data = await response.json();
 
